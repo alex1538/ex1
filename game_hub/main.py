b1cbd5107e2a01da9749c9329105d7be2de0586a
Alex <tmsd1538@gmail.com>
2024-05-17 18:42:31 +0200

    Add user_ball and rand_ball

--- game_hub/main.py
+++ game_hub/main.py
@@ -5,3 +5,8 @@ start = input("You start game 'Tik-Tak-Toe'. For start press '+' and '-' to quit
 if start == "+":
     print("Loading...")
     print("Game starting!")
+    print("3...2...1...Go!")
+    print("If you want qiut - press '-'")
+    print("If you want known your score - press '?'")
+    user_ball = 0
+    rand_ball = 0
