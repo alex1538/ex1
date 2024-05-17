e90cb32ab463ff8accf82edce4f582c9e5a85945
Alex <tmsd1538@gmail.com>
2024-05-17 19:06:53 +0200

    Patch #2

--- game_hub/main.py
+++ game_hub/main.py
@@ -19,20 +19,26 @@ if start == "+":
 
             if rand == "R" and user == "S":
                 rand_ball += 1
+                print("Ohh... You lose!")
             if rand == "S" and user == "P":
                 rand_ball += 1
+                print("Ohh... You lose!")
             if rand == "P" and user == "R":
                 rand_ball += 1
+                print("Ohh... You lose!")
 
             if rand == "P" and user == "S":
                 user_ball += 1
+                print("Yaeh! You Win!")
             if rand == "S" and user == "R":
                 user_ball += 1
+                print("Yaeh! You Win!")
             if rand == "R" and user == "P":
                 user_ball += 1
+                print("Yaeh! You Win!")
 
             if rand == user:
-                pass
+               print("Draw!")
         elif user == "?":
             print(f"Your score - {user_ball}, score your enemy - {rand_ball}")
         elif user == "-":
