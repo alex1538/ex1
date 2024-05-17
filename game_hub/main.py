893fc1f165a0c55308cec06f73fb51c22a0696b5
Alex <tmsd1538@gmail.com>
2024-05-17 18:57:07 +0200

    Game Structure

--- game_hub/main.py
+++ game_hub/main.py
@@ -16,3 +16,20 @@ if start == "+":
         if user in list_play:
             rand = random.choice(list_play)
             print(rand)
+
+            if rand == "R" and user == "S":
+                rand_ball += 1
+            if rand == "S" and user == "P":
+                rand_ball += 1
+            if rand == "P" and user == "R":
+                rand_ball += 1
+
+            if rand == "P" and user == "S":
+                user_ball += 1
+            if rand == "S" and user == "R":
+                user_ball += 1
+            if rand == "R" and user == "P":
+                user_ball += 1
+
+            if rand == user:
+                pass
